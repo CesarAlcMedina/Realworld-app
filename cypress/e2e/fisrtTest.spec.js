1d632c3a6464bf571b4edb316553510628330403
CesarAlcMedina <djcesar01@hotmail.com>
2025-08-12 18:34:58 -0400

    Mocking api response Done

--- cypress/e2e/fisrtTest.spec.js
+++ cypress/e2e/fisrtTest.spec.js
@@ -31,11 +31,30 @@ describe('Test with the backend', () => {
 
   })
 
-  it.only('Verify popular tags are displayed', () => {
+  it('Verify popular tags are displayed', () => {
     cy.get('.tag-list')
     .should('contain', 'cypress')
     .and('contain', 'automation')
     .and('contain', 'test')
+  })
+
+  it('Verify global feeds like count', () => {
+    cy.intercept('GET', 'https://conduit-api.bondaracademy.com/api/articles/feed*', '{"articles":[],"articlesCount":0}')
+    cy.intercept('GET', 'https://conduit-api.bondaracademy.com/api/articles*', {fixture: 'articles.json'})
+
+    cy.contains('Global Feed').click()
+    cy.get('app-article-list button').then(hearList => {
+      expect(hearList[0]).to.contain('1')
+      expect(hearList[1]).to.contain('5')
+    })
+
+    cy.fixture('articles.json').then(file => {
+      const articleLink = file.articles[1].slug
+      file.articles[1].favoritesCount = 6
+      cy.intercept('POST', 'https://conduit-api.bondaracademy.com/api/articles/' + articleLink + '/favorite', file)
+
+    })
+    cy.get('app-article-list button').eq(1).click().should('contain', '6')
   })
 
 })  
