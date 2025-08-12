6cd9dd9924a3aa7f177b29092d4446a9e81b49f4
CesarAlcMedina <djcesar01@hotmail.com>
2025-08-11 20:21:46 -0400

    Mocking API Response

--- cypress/e2e/fisrtTest.spec.js
+++ cypress/e2e/fisrtTest.spec.js
@@ -9,8 +9,28 @@ describe('Test with the backend', () => {
     cy.log('This is the first test');
   })
 
-  it.only('Verify the request and response', () => {
-    
+  it('Verify the request and response', () => {
+
+    cy.intercept('post', '**/api/articles').as('postArticles')
+    const unique = Date.now();
+    cy.contains('New Article').click()
+    cy.get('[formControlname="title"]').type(`This is a ${unique} `)
+    cy.get('[formControlname="description"]').type('This is a description')
+    cy.get('[formControlname="body"]').type('This is a body of the article')
+    cy.contains('Publish Article').click()
+
+    cy.wait('@postArticles',).then( xhr => {
+      console.log(xhr)
+      expect(xhr.response.statusCode).to.be.oneOf([200, 201])
+      expect(xhr.request.body.article.body).to.equal('This is a body of the article')
+      expect(xhr.request.body.article.description).to.equal('This is a description')
+
+    })
+
+  })
+
+  it.only('Verify popular tags are displayed', () => {
+
   })
 
 })  
