2175e0440a7ab82dd876775de6e32777418d092e
CesarAlcMedina <djcesar01@hotmail.com>
2025-08-12 20:22:26 -0400

    finish in home

--- cypress/e2e/fisrtTest.spec.js
+++ cypress/e2e/fisrtTest.spec.js
@@ -2,7 +2,7 @@ describe('Test with the backend', () => {
 
   
   beforeEach(('loggin to application'), () => {
-    cy.intercept('GET', 'https://conduit-api.bondaracademy.com/api/tags', {fixture: 'tags.json'})
+    cy.intercept({method:'GET', path:'tags'}, {fixture: 'tags.json'})
     cy.logginToApplication();
   })
 
@@ -28,8 +28,37 @@ describe('Test with the backend', () => {
       expect(xhr.request.body.article.description).to.equal('This is a description')
 
     })
-
   })
+
+    it.only('Intercepting and modifying the request and response', () => {
+
+    //cy.intercept('post', '**/api/articles', (req) => {
+    //  req.body.article.description = "This is a description 2"
+    //}).as('postArticles')
+
+    cy.intercept('post', '**/api/articles', (req) => {
+       req.reply(res => {
+        expect(res.body.article.description).to.equal('This is a description')
+        res.body.article.description = 'This is a description 2'
+       })
+    }).as('postArticles')
+
+    const unique = Date.now();
+    cy.contains('New Article').click()
+    cy.get('[formControlname="title"]').type(`This is a ${unique} `)
+    cy.get('[formControlname="description"]').type('This is a description')
+    cy.get('[formControlname="body"]').type('This is a body of the article')
+    cy.contains('Publish Article').click()
+
+    cy.wait('@postArticles',).then( xhr => {
+      console.log(xhr)
+      expect(xhr.response.statusCode).to.be.oneOf([200, 201])
+      expect(xhr.request.body.article.body).to.equal('This is a body of the article')
+      expect(xhr.request.body.article.description).to.equal('This is a description 2')
+
+    })
+  })
+
 
   it('Verify popular tags are displayed', () => {
     cy.get('.tag-list')
