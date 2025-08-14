34749057c7197f193b040665b10e52f13af2724e
CesarAlcMedina <djcesar01@hotmail.com>
2025-08-13 20:27:31 -0400

    almost done

--- cypress/e2e/fisrtTest.spec.js
+++ cypress/e2e/fisrtTest.spec.js
@@ -1,77 +1,74 @@
 describe('Test with the backend', () => {
 
-  
-  beforeEach(('loggin to application'), () => {
-    cy.intercept({method:'GET', path:'tags'}, {fixture: 'tags.json'})
-    cy.logginToApplication();
+  beforeEach('loggin to application', () => {
+    cy.intercept({ method: 'GET', path: 'tags' }, { fixture: 'tags.json' })
+    cy.logginToApplication()
   })
 
-
   it('first', () => {
-    cy.log('This is the first test');
+    cy.log('This is the first test')
   })
 
   it('Verify the request and response', () => {
-
     cy.intercept('post', '**/api/articles').as('postArticles')
-    const unique = Date.now();
+    
+    const unique = Date.now()
+    
     cy.contains('New Article').click()
     cy.get('[formControlname="title"]').type(`This is a ${unique} `)
     cy.get('[formControlname="description"]').type('This is a description')
     cy.get('[formControlname="body"]').type('This is a body of the article')
     cy.contains('Publish Article').click()
 
-    cy.wait('@postArticles',).then( xhr => {
+    cy.wait('@postArticles').then(xhr => {
       console.log(xhr)
       expect(xhr.response.statusCode).to.be.oneOf([200, 201])
       expect(xhr.request.body.article.body).to.equal('This is a body of the article')
       expect(xhr.request.body.article.description).to.equal('This is a description')
-
     })
   })
 
-    it.only('Intercepting and modifying the request and response', () => {
-
-    //cy.intercept('post', '**/api/articles', (req) => {
-    //  req.body.article.description = "This is a description 2"
-    //}).as('postArticles')
+  it('Intercepting and modifying the request and response', () => {
+    // cy.intercept('post', '**/api/articles', (req) => {
+    //   req.body.article.description = "This is a description 2"
+    // }).as('postArticles')
 
     cy.intercept('post', '**/api/articles', (req) => {
-       req.reply(res => {
+      req.reply(res => {
         expect(res.body.article.description).to.equal('This is a description')
         res.body.article.description = 'This is a description 2'
-       })
+      })
     }).as('postArticles')
 
-    const unique = Date.now();
+    const unique = Date.now()
+    
     cy.contains('New Article').click()
     cy.get('[formControlname="title"]').type(`This is a ${unique} `)
     cy.get('[formControlname="description"]').type('This is a description')
     cy.get('[formControlname="body"]').type('This is a body of the article')
     cy.contains('Publish Article').click()
 
-    cy.wait('@postArticles',).then( xhr => {
+    cy.wait('@postArticles').then(xhr => {
       console.log(xhr)
       expect(xhr.response.statusCode).to.be.oneOf([200, 201])
       expect(xhr.request.body.article.body).to.equal('This is a body of the article')
-      expect(xhr.request.body.article.description).to.equal('This is a description 2')
-
+      expect(xhr.response.body.article.description).to.equal('This is a description 2')
     })
   })
 
-
   it('Verify popular tags are displayed', () => {
     cy.get('.tag-list')
-    .should('contain', 'cypress')
-    .and('contain', 'automation')
-    .and('contain', 'test')
+      .should('contain', 'cypress')
+      .and('contain', 'automation')
+      .and('contain', 'test')
   })
 
   it('Verify global feeds like count', () => {
     cy.intercept('GET', 'https://conduit-api.bondaracademy.com/api/articles/feed*', '{"articles":[],"articlesCount":0}')
-    cy.intercept('GET', 'https://conduit-api.bondaracademy.com/api/articles*', {fixture: 'articles.json'})
+    cy.intercept('GET', 'https://conduit-api.bondaracademy.com/api/articles*', { fixture: 'articles.json' })
 
     cy.contains('Global Feed').click()
+    
     cy.get('app-article-list button').then(hearList => {
       expect(hearList[0]).to.contain('1')
       expect(hearList[1]).to.contain('5')
@@ -81,9 +78,54 @@ describe('Test with the backend', () => {
       const articleLink = file.articles[1].slug
       file.articles[1].favoritesCount = 6
       cy.intercept('POST', 'https://conduit-api.bondaracademy.com/api/articles/' + articleLink + '/favorite', file)
-
     })
+    
     cy.get('app-article-list button').eq(1).click().should('contain', '6')
   })
+
+  it.only('delete a new article in a global feed', () => {
+    const date = Date.now()
+    const userCredentials = {
+        "user": {
+            "email": "djtest@hotmail.com",
+            "password": "cesar0581998"
+        }
+    }
+
+    const bodyRequest = {
+      "article": {
+        "tagList": [],
+        "title": `Request from the API ${date}`,
+        "description": "API testing is easy",
+        "body": "Angular is cool"
+      }
+    }
+
+    // Obtener el token del localStorage después del login
+    cy.window().then((win) => {
+      const token = win.localStorage.getItem('jwt')
+      cy.wrap(token).as('token')
+    })
+
+    cy.request('POST', 'https://conduit-api.bondaracademy.com/api/users/login', userCredentials)
+    .its('body').then(body => {
+      const token = body.user.token
+
+      cy.request({
+        url: 'https://conduit-api.bondaracademy.com/api/articles',
+        headers: {'Authorization': 'Token '+token},
+        method: 'POST',
+        body: bodyRequest
+      }).then(response => {
+        expect(response.status).to.equal(201)
+      })
+
+      cy.contains('Global Feed').click()
+      cy.get('.article-preview').click()
+      cy.get('.article-actions').contains('Delete Article').click()
+      
+    })
+
+  })  
 
 })  
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,9 +23,22 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-Cypress.Commands.add(('logginToApplication'), () => {
-    cy.visit('/login');
-    cy.get('[placeholder="Email"]').type('cytest@test.com')
-    cy.get('[placeholder="Password"]').type('Welcome123')
-    cy.get('form').submit()
+Cypress.Commands.add('logginToApplication', () => {
+    const userCredentials = {
+        "user": {
+            "email": "djtest@hotmail.com",
+            "password": "cesar0581998"
+        }
+    }
+
+    cy.request('POST', 'https://conduit-api.bondaracademy.com/api/users/login', userCredentials)
+        .its('body').then(body => {
+            const token = body.user.token
+
+            cy.visit('/', {
+                onBeforeLoad (win) {
+                    win.localStorage.setItem('jwt', token)
+                }
+            })
+        })
 })
